582f7a1a7abd5d5d35fb506ce8b3dfef85f68c67
24HP5072 24HP5072 <24HP5072@admins-iMac-2.local>
2025-03-15 11:24:55 +0800

    fix

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -23,7 +23,7 @@ const Page = () => {
                     </div>
                     {selected === "accinfo" && <p>You selected Option 1!</p>}
                     {selected === "package" && <PackageForm></PackageForm>}
-                    {selected === "instruction" && <p>sda</p>}
+                    {selected === "instruction" && <p>icltspmo</p>}
                     {selected === "" && <UserInfo></UserInfo>}                                      
         </div>
     )
