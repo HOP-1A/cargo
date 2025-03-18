ec871c4cd6594cd14e45b280e35802ec0d6ae01b
24HP0212 24HP0212 <24HP0212@admins-iMac.local>
2025-03-18 15:43:00 +0800

    zarim ymig ynzlav

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,23 +1,62 @@
 "use client";
+import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [darkMode, setDarkMode] = useState(
+    localStorage.getItem("theme") === "dark"
+  );
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [darkMode]);
+
   return (
-      <div className="whiteheader flex-column">
-        <nav className="flexrow">
-          <a href="index.php" className="nav-logo">
-            <img src="https://www.cargoservice.mn/img/logo-black.png" alt="A&amp;N" />
-          </a>
-          <div className="flexrow">
-          <div className="nav-item">Эхлэл</div>
-          <div className="nav-item">Тооцоолуур</div>
-          <div className="nav-item">Заавар</div>
-          <img 
-            src="https://static-00.iconduck.com/assets.00/dark-theme-icon-2048x2048-ymrfkxsy.png" 
-            alt="Dark and Light Mode Icon" 
-            className="round-image"
-          />
-          </div>
-        </nav>
-      </div>
+    <div className="whiteheader flex-column">
+      <nav className="flexrow">
+        <a className="nav-logo">
+          <Link href={"/"}>
+            <img
+              src="https://www.cargoservice.mn/img/logo-black.png"
+              alt="A&amp;N"
+            />
+          </Link>
+        </a>
+        <div className="flexrow">
+          <Link href={"/"}>
+            <div className="nav-item">Эхлэл</div>
+          </Link>
+          <Link href={"/tootsooluur"}>
+            <div className="nav-item">Тооцоолуур</div>
+          </Link>
+          <Link href={"/zaavar"}>
+            <div className="nav-item">Заавар</div>
+          </Link>
+          <Link href={"/user"}>
+            <img
+              className="rounded-full w-[40px] cursor-pointer"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2TgOv9CMmsUzYKCcLGWPvqcpUk6HXp2mnww&s"
+            />
+          </Link>
+
+          <button
+            onClick={() => setDarkMode(!darkMode)}
+            className="p-3 rounded-full cursor-pointer"
+          >
+            <img
+              src="https://static-00.iconduck.com/assets.00/dark-theme-icon-2048x2048-ymrfkxsy.png"
+              alt="Dark and Light Mode Icon"
+              className="w-10 h-10"
+            />
+          </button>
+        </div>
+      </nav>
+    </div>
   );
 }
---
+++ src/app/zaavar/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center">Zaavar bgashu za</div>
+  );
+}
