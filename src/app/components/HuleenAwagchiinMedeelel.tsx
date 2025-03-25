7c6ad63003bff506f7139dba31bfa299545bfa49
24HP4966 24HP4966 <24HP4966@admins-iMac.local>
2025-03-25 18:31:17 +0800

    m

---
+++ src/app/components/HuleenAwagchiinMedeelel.tsx
@@ -0,0 +1,34 @@
+import { Card, CardTitle } from "@/components/ui/card";
+import { useState } from "react";
+
+export const HuleenAwagchiinMedeelel = () => {
+  const [awagchiinNer, setAwagchiinNer] = useState("");
+  const [awagchiinDugaar, setawagchiinDugaar] = useState();
+  return (
+    <>
+      <Card className="py-5 px-8 w-[480px] h-full ">
+        <CardTitle className="text-2xl pb-10 text-left">
+          Хүлээн авагчийн мэдээлэл
+        </CardTitle>
+        <div className="flex gap-5">
+          <div className="flex flex-col space-y-1.5">
+            <p>Нэр</p>
+            <input
+              type="text"
+              className=" border-1 border-gray-300 py-2 px-4 w-[192.5px] rounded-sm"
+              placeholder="asdf"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col space-y-1.5">
+          <p>Утасны дугаар</p>
+          <input
+            type="text"
+            className=" border-1 border-gray-300 py-2 px-4 w-[405px] rounded-sm"
+            placeholder="asdf"
+          />
+        </div>
+      </Card>
+    </>
+  );
+};
